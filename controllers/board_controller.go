01b7575e0b9b9c4b2a0946bb6dbce6eb7f071323
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-07-03 20:45:39 +0800

    Delete Board Member

--- controllers/board_controller.go
+++ controllers/board_controller.go
@@ -88,3 +88,18 @@ func (c *BoardController) AddBoardMembers(ctx *fiber.Ctx) error {
 
 	return utils.Success(ctx, "Members berhasil ditambahkan", nil)
 }
+
+func (c *BoardController) RemoveBoardMembers(ctx *fiber.Ctx) error {
+	publicID := ctx.Params("id")
+
+	var userIDs []string
+	if err := ctx.BodyParser(&userIDs); err != nil {
+		return utils.BadRequest(ctx, "Gagal parsing data", err.Error())
+	}
+
+	if err := c.services.RemoveMembers(publicID, userIDs); err != nil {
+		return utils.BadRequest(ctx, "Gagal menghapus members", err.Error())
+	}
+
+	return utils.Success(ctx, "Board members berhasil dihapus", nil)
+}
